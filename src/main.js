bfb8d94019d8410d100cfd1b47fadd58e36fc1c1
Mark Duarte <duarte.mark8@gmail.com>
2023-01-24 13:23:13 -0800

    made the code per the module

--- src/main.js
+++ src/main.js
@@ -3,3 +3,12 @@ const { welcome, goodbye, tell } = require("../utils/fortune-teller");
 const promise = welcome();
 
 console.log(promise);
+
+const question = "Eat my shorts?";
+const tellPromise = tell(question);
+tellPromise
+    .then((fortune) => {
+        console.log(question);
+        console.log(fortune);
+    })
+    .catch(console.error)
